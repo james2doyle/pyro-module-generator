b537b905090a041f0620883dd57a9d7b97419d11
james2doyle <james2doyle@gmail.com>
2013-04-17 13:46:24 -0400

    add finished/complete view

---
+++ application/views/home/finished.blade.php
@@ -0,0 +1,10 @@
+@include('partials.header')
+<div class="boxed">
+<header class="center">
+<h1>PyroCMS Module Generator</h1>
+<h4>Created by <a target="_blank" href="https://twitter.com/james2doyle">James Doyle</a></h4>
+<h5>Version 1.1</h5>
+</header>
+<h4>The <strong><?php echo $module; ?></strong> module has been generated. Find it in the application/generator/generated/<?php echo $module; ?></h4>
+</div>
+@include('partials.footer')
