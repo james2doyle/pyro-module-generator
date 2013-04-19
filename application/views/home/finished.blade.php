3be89a5e73290311344c5d526c1d188288cb4a07
james2doyle <james2doyle@gmail.com>
2013-04-19 11:56:48 -0400

    conditionally show zip link

--- application/views/home/finished.blade.php
+++ application/views/home/finished.blade.php
@@ -5,7 +5,10 @@
 <h4>Created by <a target="_blank" href="https://twitter.com/james2doyle">James Doyle</a></h4>
 <h5>Version 1.2</h5>
 </header>
-<h4>The <strong><?php echo $module; ?></strong> module has been generated. Download <a href="<?php
+<h4>The <strong><?php echo $module; ?></strong> module has been generated.
+<?php if ($zip): ?>
+Download <a href="<?php
 echo $url ?>/generated/<?php echo $module; ?>.zip"><?php echo $module; ?>.zip</a></h4>
+<?php endif; ?>
 </div>
 @include('partials.footer')
