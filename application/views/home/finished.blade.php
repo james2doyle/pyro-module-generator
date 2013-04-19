e56ece991262fcb1f7347abc90febd1483ff3aaa
james2doyle <james2doyle@gmail.com>
2013-04-19 11:36:08 -0400

    add download link for generated zip file

--- application/views/home/finished.blade.php
+++ application/views/home/finished.blade.php
@@ -5,6 +5,7 @@
 <h4>Created by <a target="_blank" href="https://twitter.com/james2doyle">James Doyle</a></h4>
 <h5>Version 1.1</h5>
 </header>
-<h4>The <strong><?php echo $module; ?></strong> module has been generated. Find it in the application/generator/generated/<?php echo $module; ?></h4>
+<h4>The <strong><?php echo $module; ?></strong> module has been generated. Download <a href="<?php
+echo $url ?>/generated/<?php echo $module; ?>.zip"><?php echo $module; ?>.zip</a></h4>
 </div>
 @include('partials.footer')
